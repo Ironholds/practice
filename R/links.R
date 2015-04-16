90b6c2343a0e994cbb35d8b28167957d2b4fd712
ironholds <ironholds@gmail.com>
2015-04-15 23:27:40 -0400

    add depended_packages

---
+++ R/links.R
@@ -0,0 +1,24 @@
+#'@title identify packages that a package mentions
+#'@description this identifies unique packages that are linked to by the package
+#'that is being evaluated, in the context of dependencies, suggests and LinkingTo fields.
+#'
+#'@param package_metadata package metadata retrieved with \code{\link{get_package_metadata}} 
+#'
+#'@param with_versions whether to return the version number (or lack thereof) as well as the
+#'mentioned package's name. Set to FALSE by default.
+#'
+#'@return a vector of package names, if \code{with_versions} is FALSE, or a data.frame
+#'of "packag" and "version", if TRUE.
+#'
+#'@export
+depended_packages <- function(package_metadata, with_versions = FALSE){
+  last_release <- package_metadata$versions[[length(package_metadata$versions)]]
+  
+  if(with_versions){
+    versioned_vec <- unlist(c(last_release$Depends, last_release$Imports, last_release$Suggests))
+    versioned <- as.data.frame(package = names(versioned_vec), version = versioned_vec,
+                               stringsAsFactors = FALSE)
+    return(versioned[!duplicated(versioned),])
+  }
+  return(unique(c(names(last_release$Depends),names(last_release$Imports), names(last_release$Suggests))))
+}
