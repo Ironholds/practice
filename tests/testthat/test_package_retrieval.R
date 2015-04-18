d31a3130bea601b3c3fba027df8adf4e6c47da60
ironholds <ironholds@gmail.com>
2015-04-18 11:00:55 -0400

    thanks, Stefan!

--- tests/testthat/test_package_retrieval.R
+++ tests/testthat/test_package_retrieval.R
@@ -12,11 +12,11 @@ test_that("Package names can be retrieved", {
   expect_true(is.vector(package_names, "character"))
 })
 
-# test_that("Package metadata can be retrieved", {
-#   metadata <- get_package_metadata("ggplot2")
-#   expect_true(is.list(metadata))
-#   expect_that(names(metadata), equals(c("_id","_rev","name","versions","timeline","latest","title","archived")))
-# })
+test_that("Package metadata can be retrieved", {
+  metadata <- get_package_metadata("ggplot2")
+  expect_true(is.list(metadata))
+  expect_that(names(metadata), equals(c("_id","_rev","name","versions","timeline","latest","title","archived", "revdeps")))
+})
 
 test_that("Package content can be retrieved", {
   result <<- get_package_source("ggplot2")
