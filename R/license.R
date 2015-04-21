5fa962543ce40165bae911d8d1cd7df442fe12d5
ironholds <ironholds@gmail.com>
2015-04-21 10:50:29 -0400

    bugs are gr8

--- R/license.R
+++ R/license.R
@@ -10,10 +10,17 @@
 #'@return a character vector containing each unique license mentioned in the
 #'package's DESCRIPTION field.
 #'
+#'@examples
+#'\dontrun{
+#'#What license(s) are urltools under?
+#'get_license(get_package_metadata("urltools"))
+#'}
+#'
 #'@export
 get_license <- function(package_metadata){
   package_metadata <- check_metadata(package_metadata)
   license_info <- package_metadata$versions[[length(package_metadata$versions)]]$License
   license_info <- gsub(x = license_info, pattern = "(\\||\\+) file LICENSE", replacement = "")
   licenses <- unlist(strsplit(license_info, split = "|", fixed = TRUE))
+  return(licenses)
 }
