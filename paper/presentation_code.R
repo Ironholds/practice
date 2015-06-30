71f322fe66acf0de0f2f6e255ae7413fa4a6a288
Oliver Keyes <Ironholds@users.noreply.github.com>
2015-06-30 16:15:14 +0200

    Rework on phone

--- paper/presentation_code.R
+++ paper/presentation_code.R
@@ -1,7 +1,13 @@
 library(ggplot2)
 library(ggthemes)
+library(data.table)
 load("./data/CRANpractices.rda")
-results <- as.data.frame(table(CRANpractices$testing))
+
+data <- as.data.table(CRANpractices)
+results <- data[,j=list(packages=.N), by='testing']
+
 ggsave(filename = "./paper/basic_usage.svg",
-       plot = ggplot(results, aes(reorder(Var1, Freq), Freq)) + geom_bar(stat="identity", fill = "steelblue1") + coord_flip() +
-         labs(title = "Unit test usage in CRAN packages", x = "Foo", y = "Bar") + theme_fivethirtyeight())
+       plot = ggplot(results, aes(reorder(testing, packages), packages)) + 
+                geom_bar(stat="identity", fill = "steelblue1") + coord_flip() +
+                labs(title = "Unit test usage in CRAN packages", x = "Testing Approach", y = "Number of packages") +
+                theme_fivethirtyeight())
