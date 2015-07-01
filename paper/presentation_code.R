ca36c114752900c0fb63296ca2dc56faaac9bc21
Oliver Keyes <Ironholds@users.noreply.github.com>
2015-07-01 10:07:13 +0200

    More mobile code

--- paper/presentation_code.R
+++ paper/presentation_code.R
@@ -1,9 +1,13 @@
 library(ggplot2)
 library(ggthemes)
+library(lubridate)
 library(data.table)
 load("./data/CRANpractices.rda")
 
 data <- as.data.table(CRANpractices)
+data$first_release <- lubridate::year(data$first_release)
+data$last_release <- lubridate::year(data$last_release)
+
 results <- data[,j=list(packages=.N), by='testing']
 
 ggsave(filename = "./paper/basic_usage.svg",
@@ -11,3 +15,7 @@ ggsave(filename = "./paper/basic_usage.svg",
                 geom_bar(stat="identity", fill = "steelblue1") + coord_flip() +
                 labs(title = "Unit test usage in CRAN packages", x = "Testing Approach", y = "Number of packages") +
                 theme_fivethirtyeight())
+
+data$has_any_tests <- !(data$testing == "None")
+initial_results <- data[,j=list(packages={
+    sum(.SD$has_any_tests)/.N}), by= "first_release"]
